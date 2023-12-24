b39966e776c2e29591532dedfb7ad0d84f12fbe3
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2023-12-24 11:37:04 -0500

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,12 +1,10 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
+
 public class AddTwo {
 	public static void main(String[] args) {
 		// Put your code here	
 		int x = Integer.parseInt(args[0]);
 		int z = Integer.parseInt(args[1]);
 		int sum = x + z;
-		System.out.println(x +" + " + z + " = " + sum);
+		System.out.println(x + " + " + z + " = " + sum);
 	}
 }
