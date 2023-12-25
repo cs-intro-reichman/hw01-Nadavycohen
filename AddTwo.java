7ab9d1acdad514134139df9ff0121994bebbc749
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2023-12-24 19:04:18 -0500

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,10 +1,9 @@
-
 public class AddTwo {
 	public static void main(String[]args) {
 		// Put your code here	
 		int x = Integer.parseInt(args[0]);
 		int w = Integer.parseInt(args[1]);
-		int z = x+y;
+		int z = x+w;
 		System.out.println(x + " + " + w + " = " + z);
 	}
 }
