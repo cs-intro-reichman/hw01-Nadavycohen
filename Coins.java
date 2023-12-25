85611b62f8683d585cc6c2427e0d1eb7169bc32d
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2023-12-24 19:52:30 -0500

    Coins.java

--- Coins.java
+++ Coins.java
@@ -5,5 +5,11 @@
 public class Coins {
 	public static void main(String[] args) {
 		// Put your code here
+		int quarters = 25;
+		int money = Integer.parseInt(args[0]);
+		int a = money/quarters;
+		int b = money%quarters;
+		System.out.println("use " + a + " quarters and " + b + " cents");
+
 	}
 }
